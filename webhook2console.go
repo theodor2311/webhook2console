d39a559c9824782b6f4af13c15deb731d4990df0
Theodor Li <theodor2311@gmail.com>
2023-02-15 14:48:08 +0800

    Create webhook2console.go

---
+++ webhook2console.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		}
+		fmt.Println(string(body))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
